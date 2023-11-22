246ead224ddce25b8e9bac77c579c9cdfd39beed
Javier <garcia.luzanilla.javier@gmail.com>
2023-11-21 17:25:44 -0800

    sin errores

--- new_client/src/App.jsx
+++ new_client/src/App.jsx
@@ -1,45 +1,31 @@
+import { Route, Routes } from "react-router-dom";
 
-import {Route,Routes} from "react-router-dom"
+import Layout from "./components/layout.jsx";
+import Navbar from "./components/navbar.jsx";
+import Inicio from "./screens/inicio.jsx";
+import Autos from "./screens/autos.jsx";
 
+import Register from "./screens/register.jsx";
 
-import Layout from './components/layout.jsx'
-import Navbar from './components/navbar.jsx'
-import Inicio from "./screens/inicio.jsx"
-import Autos from "./screens/autos.jsx"
-
-import Register from './screens/register.jsx'
-import Image from './screens/images.jsx'
-import Foot from "./components/foot.jsx"
-
-
+import Foot from "./components/foot.jsx";
 
 function App() {
-
-
   return (
     <>
-    <div className="bg-gradient-to-t from-pink-900 via-purple-900 to-indigo-950 min-h-screen ">
-      <Navbar/>
+      <div className="bg-gradient-to-t from-pink-900 via-purple-900 to-indigo-950 min-h-screen ">
+        <Navbar />
 
-
-      <Layout>
-  
-      <Routes>
-        <Route path='/' element={<Inicio></Inicio>}></Route>
-        <Route path='/autos' element={<Autos></Autos>}></Route>
-        <Route path='/register' element={<Register></Register>}>
-
-      
-        </Route>
-        </Routes>
-     
-      
-      </Layout>
-      
-    </div>
-    <Foot></Foot>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Inicio></Inicio>}></Route>
+            <Route path="/autos" element={<Autos></Autos>}></Route>
+            <Route path="/register" element={<Register></Register>}></Route>
+          </Routes>
+        </Layout>
+      </div>
+      <Foot></Foot>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
