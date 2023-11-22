d54f8aac13b8122c9bd9c93f50d95691b1e41efc
Javier <garcia.luzanilla.javier@gmail.com>
2023-11-21 17:24:23 -0800

    Nomas otra imagen de carro

--- new_client/src/screens/autos.jsx
+++ new_client/src/screens/autos.jsx
@@ -21,7 +21,7 @@ const Autos = () => {
       </div>
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content"/>
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://japantuningsudamerica.com/wp-content/uploads/2017/12/WV-01.jpg" alt="content"/>
           <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
           <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
           <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
