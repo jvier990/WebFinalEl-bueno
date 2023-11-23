ce3d4395d30c38d4cae8e052703589d386b11766
Javier <garcia.luzanilla.javier@gmail.com>
2023-11-23 06:20:19 -0800

    boton desgraciado

--- new_client/src/main.jsx
+++ new_client/src/main.jsx
@@ -1,18 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Button from './components/button.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-    
-    <Button></Button>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
