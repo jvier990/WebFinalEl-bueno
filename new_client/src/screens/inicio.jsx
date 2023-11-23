18641e134b7479f09c5ac3984bb76971269ab27c
Javier <garcia.luzanilla.javier@gmail.com>
2023-11-23 11:19:14 -0800

    ahora si

--- new_client/src/screens/inicio.jsx
+++ new_client/src/screens/inicio.jsx
@@ -80,7 +80,7 @@ const Inicio = () => {
               Contraseña
             </label>
             <input
-              type="text"
+              type="password"
               id="full-name"
               name="password"
               class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
